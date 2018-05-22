71c011c61f9f3eec8e403bd81eed441740009711
joaopedrodcf <joaoferr93@gmail.com>
2018-05-23 00:51:47 +0100

    Changed the box-shadow of footer

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Container = styled.footer`
   grid-area: footer;
   background-color: #006494;
-  box-shadow: 1px 1px 10px 1px gray;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
   padding: 20px 0;
   display: flex;
   flex-direction: column;
