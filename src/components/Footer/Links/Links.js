e5f6eb84f21394154ae1fadf1cd577c34d8c5337
joaopedrodcf <joaoferr93@gmail.com>
2018-09-30 01:12:12 +0100

    feat: Add medium and twitter to the footer

--- src/components/Footer/Links/Links.js
+++ src/components/Footer/Links/Links.js
@@ -7,8 +7,14 @@ const Links = () => (
         <Link href="https://github.com/joaopedrodcf">
             <i className="fab fa-github fa-2x" />
         </Link>
-        <Link href="https://www.linkedin.com/in/joaoferr93/">
+        <Link href="https://www.linkedin.com/in/joaoferr93">
             <i className="fab fa-linkedin fa-2x" />
+        </Link>
+        <Link href="https://twitter.com/Joaopedrodcf">
+            <i className="fab fa-twitter-square fa-2x" />
+        </Link>
+        <Link href="https://medium.com/@joaoferr93">
+            <i className="fab fa-medium fa-2x" />
         </Link>
     </div>
 );
