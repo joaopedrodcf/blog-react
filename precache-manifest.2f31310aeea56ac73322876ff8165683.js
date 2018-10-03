7621d86b7b1cfd39301c44bba4061d988ce3af2f
joaopedrodcf <joaoferr93@gmail.com>
2018-10-03 17:11:10 +0100

    Updates

---
+++ precache-manifest.2f31310aeea56ac73322876ff8165683.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d2ea7c3212d20405832338505ef5c0",
+    "url": "/blog-react/static/media/logo.d6d2ea7c.png"
+  },
+  {
+    "revision": "eb20b7b0e2b4e6211af8",
+    "url": "/blog-react/static/js/runtime~main.eb20b7b0.js"
+  },
+  {
+    "revision": "fb6f3df11dc5e3a950ea",
+    "url": "/blog-react/static/js/main.fb6f3df1.chunk.js"
+  },
+  {
+    "revision": "0511b504b0868d86aec2",
+    "url": "/blog-react/static/js/1.0511b504.chunk.js"
+  },
+  {
+    "revision": "14a4199d6b95f7b55cb07650f4feeaa3",
+    "url": "/blog-react/index.html"
+  }
+];
